a7986af94ed3505fed7fa21cf213a1fdde6f9880
Antti Kekki <antti.kekki@reaktor.com>
2020-12-01 19:08:36 +0200

    Day 1 puzzle 2

--- src/1/1.ts
+++ src/1/1.ts
@@ -5,11 +5,27 @@ const filePath = path.resolve(__dirname, "../../resources/1/data.txt")
 const file = fs.readFileSync(filePath)
 const data = new String(file).split("\n").map(Number)
 
-data.forEach((value1, i1) => {
-  data.forEach((value2, i2) => {
+console.log("Day 1 puzzle 1:")
+data.forEach((value1) => {
+  data.forEach((value2) => {
     if (value1 + value2 === 2020) {
       console.log(`${value1} + ${value2} = 2020`)
       console.log(`${value1} * ${value2} = ${value1 * value2}`)
     }
+  })
+})
+
+console.log("\n")
+console.log("Day 1 puzzle 2:")
+data.forEach((value1) => {
+  data.forEach((value2) => {
+    data.forEach((value3) => {
+      if (value1 + value2 + value3 === 2020) {
+        console.log(`${value1} + ${value2} + ${value3} = 2020`)
+        console.log(
+          `${value1} * ${value2} * ${value3} = ${value1 * value2 * value3}`
+        )
+      }
+    })
   })
 })
