7e4c5e53fe5c12a3be61bd9501908f388f81d492
Antti Kekki <antti.kekki@reaktor.com>
2021-12-05 21:33:08 +0200

    2021 day 4 puzzle 2

--- src/2021/4.ts
+++ src/2021/4.ts
@@ -1,7 +1,12 @@
 import { loadFileRows } from "../util"
 
 type Cell = { value: number; match: boolean }
-type Table = Array<Array<Cell>>
+type Table = {
+  table: Array<Array<Cell>>
+  won: boolean
+  wonAtRound: number
+  wonAtNumber: number
+}
 
 const parseTables = (data: Array<string>) => {
   const numbers = data[0].split(",").map(Number)
@@ -15,7 +20,12 @@ const parseTables = (data: Array<string>) => {
       }
     } else {
       if (!currentTable) {
-        currentTable = []
+        currentTable = {
+          table: [],
+          won: false,
+          wonAtNumber: NaN,
+          wonAtRound: NaN
+        }
         tables.push(currentTable)
       }
       const tableRow = row
@@ -25,14 +35,14 @@ const parseTables = (data: Array<string>) => {
         .map((v) => {
           return { value: v, match: false }
         })
-      currentTable.push(tableRow)
+      currentTable.table.push(tableRow)
     }
   }
 
   // Validate
   for (const table of tables) {
-    console.assert(table.length === 5, "Every table should have 5 rows")
-    for (const row of table) {
+    console.assert(table.table.length === 5, "Every table should have 5 rows")
+    for (const row of table.table) {
       console.assert(row.length === 5, "Every table row should have 5 columns")
     }
   }
@@ -40,14 +50,15 @@ const parseTables = (data: Array<string>) => {
   return { numbers, tables }
 }
 
-const executeBingo = (
-  tables: Array<Table>,
-  numbers: Array<number>
-): { table: Table; number: number } | undefined => {
+const executeBingo = (tables: Array<Table>, numbers: Array<number>) => {
+  let round = 1
   for (const number of numbers) {
     // Mark matches
     for (const table of tables) {
-      for (const row of table) {
+      if (table.won) {
+        continue
+      }
+      for (const row of table.table) {
         for (const rowNumber of row) {
           if (rowNumber.value === number) {
             rowNumber.match = true
@@ -57,26 +68,38 @@ const executeBingo = (
     }
 
     for (const table of tables) {
+      if (table.won) {
+        continue
+      }
+
       // Check horizontal lines
-      for (const row of table) {
+      for (const row of table.table) {
         if (row.every((v) => v.match)) {
-          return { table, number }
+          table.won = true
+          table.wonAtNumber = number
+          table.wonAtRound = round
         }
       }
 
       // Check vertical lines
       for (let i = 0; i < 5; i++) {
-        if (table.map((row) => row[i]).every((v) => v.match)) {
-          return { table, number }
+        if (table.table.map((row) => row[i]).every((v) => v.match)) {
+          table.won = true
+          table.wonAtNumber = number
+          table.wonAtRound = round
         }
       }
     }
+    round++
   }
-  return undefined
 }
 
 const printTable = (table: Table) => {
   console.log("\n")
+  console.log("Won:", table.won)
+  console.log("Won at round:", table.wonAtRound)
+  console.log("Won at number:", table.wonAtNumber)
+
   const formatCell = (cell: Cell) => {
     const value =
       cell.value.toString().length === 1
@@ -84,7 +107,7 @@ const printTable = (table: Table) => {
         : cell.value.toString()
     return cell.match ? `*${value}*` : ` ${value} `
   }
-  for (const row of table) {
+  for (const row of table.table) {
     console.log(row.map(formatCell).join(" "))
   }
 }
@@ -96,24 +119,46 @@ const printGameState = (tables: Array<Table>, number: number) => {
   }
 }
 
-const calculate = (data: Array<string>) => {
-  const { numbers, tables } = parseTables(data)
-
-  const winner = executeBingo(tables, numbers)
-  if (winner) {
-    let unmatchedSum = 0
-    for (const row of winner.table) {
-      for (const rowNumber of row) {
-        if (!rowNumber.match) {
-          unmatchedSum += rowNumber.value
-        }
+const calculatePoints = (table: Table) => {
+  let unmatchedSum = 0
+  for (const row of table.table) {
+    for (const rowNumber of row) {
+      if (!rowNumber.match) {
+        unmatchedSum += rowNumber.value
       }
     }
-    const result = unmatchedSum * winner.number
-    //printGameState(tables, winner.number)
-    return { result, lastNumber: winner.number, unmatchedSum }
   }
-  return undefined
+  const result = unmatchedSum * table.wonAtNumber!
+  //printGameState(tables, winner.number)
+  return { result, lastNumber: table.wonAtNumber, unmatchedSum }
+}
+
+const findFirstWinner = (tables: Array<Table>) => {
+  const sorted = [...tables]
+    .filter((t) => t.won)
+    .sort((a, b) => a.wonAtRound - b.wonAtRound)
+  return sorted[0]
+}
+
+const findLastWinner = (tables: Array<Table>) => {
+  const sorted = [...tables]
+    .filter((t) => t.won)
+    .sort((a, b) => a.wonAtRound - b.wonAtRound)
+  return sorted[sorted.length - 1]
+}
+
+const executeBingoAndFindFirstWinner = (data: Array<string>) => {
+  const { numbers, tables } = parseTables(data)
+  executeBingo(tables, numbers)
+  const winner = findFirstWinner(tables)
+  return calculatePoints(winner)
+}
+
+const executeBingoAndFindLastWinner = (data: Array<string>) => {
+  const { numbers, tables } = parseTables(data)
+  executeBingo(tables, numbers)
+  const winner = findLastWinner(tables)
+  return calculatePoints(winner)
 }
 
 /**
@@ -122,15 +167,27 @@ const calculate = (data: Array<string>) => {
 export const day4_2021 = () => {
   console.log("2021 Day 4 puzzle 1 example:")
   const exampleData = loadFileRows(2021, 4, "example.txt")
-  const result1 = calculate(exampleData)
-  console.log("Last number (should be 24):", result1?.lastNumber)
-  console.log("Unmatched sum (should be 188):", result1?.unmatchedSum)
-  console.log("Result (should be 4512):", result1?.result)
+  const result1 = executeBingoAndFindFirstWinner(exampleData)
+  console.log("Last number (should be 24):", result1.lastNumber)
+  console.log("Unmatched sum (should be 188):", result1.unmatchedSum)
+  console.log("Result (should be 4512):", result1.result)
 
   console.log("\n2021 Day 4 puzzle 1:")
   const data = loadFileRows(2021, 4, "data.txt")
-  const result2 = calculate(data)
-  console.log("Last number:", result2?.lastNumber)
-  console.log("Unmatched sum:", result2?.unmatchedSum)
-  console.log("Result:", result2?.result)
+  const result2 = executeBingoAndFindFirstWinner(data)
+  console.log("Last number:", result2.lastNumber)
+  console.log("Unmatched sum:", result2.unmatchedSum)
+  console.log("Result:", result2.result)
+
+  console.log("\n2021 Day 4 puzzle 2 example:")
+  const result3 = executeBingoAndFindLastWinner(exampleData)
+  console.log("Last number (should be 13):", result3.lastNumber)
+  console.log("Unmatched sum (should be 148):", result3.unmatchedSum)
+  console.log("Result (should be 1924):", result3.result)
+
+  console.log("\n2021 Day 4 puzzle 2:")
+  const result4 = executeBingoAndFindLastWinner(data)
+  console.log("Last number:", result4.lastNumber)
+  console.log("Unmatched sum:", result4.unmatchedSum)
+  console.log("Result:", result4.result)
 }
