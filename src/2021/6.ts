118b4d9f1c151b1e404a989654f93b1cd5351d25
Antti Kekki <antti.kekki@reaktor.com>
2021-12-06 18:49:05 +0200

    2021 day 6 puzzle 2

--- src/2021/6.ts
+++ src/2021/6.ts
@@ -1,25 +1,39 @@
 import { loadFileRows } from "../util"
 
+type Stat = { number: number; count: number }
+
 const calculate = (data: Array<string>, days: number, printLog: boolean) => {
-  let fishes: Array<number> = data[0].split(",").map(Number)
-  printLog ? console.log("Initial state: ", fishes.join(", ")) : undefined
+  const startingFishes: Array<number> = data[0].split(",").map(Number)
+  const stats: Array<Stat> = []
+  for (const fish of startingFishes) {
+    const stat = stats.find((s) => s.number === fish)
+    if (stat) {
+      stat.count++
+    } else {
+      stats.push({ number: fish, count: 1 })
+    }
+  }
+  //console.log("Starting stats:", stats)
 
   for (let day = 1; day <= days; day++) {
-    let newFishes: Array<number> = []
-    fishes = fishes.map((fish) => {
-      if (fish === 0) {
-        newFishes.push(8)
-        return 6
+    let newFishes = 0
+    for (const stat of stats) {
+      if (stat.number === 0) {
+        newFishes += stat.count
+        stat.number = 6
+        continue
       }
-      fish--
-      return fish
-    })
-    fishes = [...fishes, ...newFishes]
-    printLog
-      ? console.log("After", day, " day: ", fishes.join(", "))
-      : undefined
+      stat.number--
+    }
+    if (newFishes > 0) {
+      stats.push({ number: 8, count: newFishes })
+    }
+    printLog ? console.log("Day ", day, ":", stats) : undefined
   }
-  return { fishes }
+  const fishesCount = stats
+    .map((f) => f.count)
+    .reduce((acc, value) => acc + value, 0)
+  return { fishesCount }
 }
 
 /**
@@ -30,14 +44,23 @@ export const day6_2021 = () => {
   const exampleData = loadFileRows(2021, 6, "example.txt")
   console.log(
     "Result (should be 26):",
-    calculate(exampleData, 18, true).fishes.length
+    calculate(exampleData, 18, false).fishesCount
   )
   console.log(
     "Result (should be 5934):",
-    calculate(exampleData, 80, false).fishes.length
+    calculate(exampleData, 80, false).fishesCount
   )
 
   console.log("\n2021 Day 6 puzzle 1:")
   const data = loadFileRows(2021, 6, "data.txt")
-  console.log("Result:", calculate(data, 80, false).fishes.length)
+  console.log("Result:", calculate(data, 80, false).fishesCount)
+
+  console.log("\n2021 Day 6 puzzle 2 example:")
+  console.log(
+    "Result (should be 26984457539):",
+    calculate(exampleData, 256, false).fishesCount
+  )
+
+  console.log("\n2021 Day 6 puzzle 2:")
+  console.log("Result:", calculate(data, 256, false).fishesCount)
 }
