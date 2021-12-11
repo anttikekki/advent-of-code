ea512b38f958f94740415f08dad9653e25394f2b
Antti Kekki <antti.kekki@reaktor.com>
2021-12-11 22:07:54 +0200

    2021 day 10 puzzle 2

--- src/2021/10.ts
+++ src/2021/10.ts
@@ -24,19 +24,20 @@ const isStartChar = (char: string): char is StartChars =>
 const isStopChar = (char: string): char is StopChars =>
   stopChars.includes(char as StopChars)
 
-const getNextValidChar = (char: AllChars): StopChars | undefined => {
-  if (isStartChar(char)) {
-    return commandTypes.find((c) => c.start === char)?.stop
-  }
-  return undefined
-}
-
 const getStartChar = (char: StopChars): StartChars =>
   commandTypes.find((c) => c.stop === char)!.start
 
-const validateRowAndReturnInvalidChar = (
-  row: string
-): StopChars | undefined => {
+const getStopChar = (char: StartChars): StopChars =>
+  commandTypes.find((c) => c.start === char)!.stop
+
+type RowResult = {
+  isInvalid: boolean
+  invalidChar?: StopChars
+  isIncomplete: boolean
+  addedStopChars?: StopChars[]
+}
+
+const validateRow = (row: string): RowResult => {
   const commandStack: Array<AllChars> = []
   console.log("\n", row)
   const chars = row.split("")
@@ -62,15 +63,29 @@ const validateRowAndReturnInvalidChar = (
           "but expected prev char would be",
           expectedPrevChar
         )
-        return char
+        return { isInvalid: true, invalidChar: char, isIncomplete: false }
       }
     }
     i++
   }
-  return undefined
+
+  if (commandStack.length > 0) {
+    const addedStopChars: StopChars[] = []
+
+    for (const incompleteStartChar of commandStack) {
+      if (isStartChar(incompleteStartChar)) {
+        const stopChar = getStopChar(incompleteStartChar)
+        addedStopChars.push(stopChar)
+      }
+    }
+    addedStopChars.reverse()
+    return { isInvalid: false, isIncomplete: true, addedStopChars }
+  }
+
+  return { isInvalid: false, isIncomplete: false }
 }
 
-const calculate = (data: Array<string>) => {
+const findInvalidLinesAndCountPoints = (data: Array<string>) => {
   const invalidCommandCounter: Record<StopChars, number> = {
     ")": 0,
     "]": 0,
@@ -78,9 +93,9 @@ const calculate = (data: Array<string>) => {
     ">": 0
   }
   for (const row of data) {
-    const result = validateRowAndReturnInvalidChar(row)
-    if (result) {
-      invalidCommandCounter[result]++
+    const result = validateRow(row)
+    if (result.isInvalid) {
+      invalidCommandCounter[result.invalidChar!]++
     }
   }
 
@@ -93,17 +108,82 @@ const calculate = (data: Array<string>) => {
   return { result }
 }
 
+const findIncompleteLinesAndCountPoints = (data: Array<string>) => {
+  const incompleteRowsAddedChars: {
+    row: string
+    addedStopChars: StopChars[]
+  }[] = []
+
+  for (const row of data) {
+    const result = validateRow(row)
+    if (result.isIncomplete && result.addedStopChars!.length > 0) {
+      incompleteRowsAddedChars.push({
+        row,
+        addedStopChars: result.addedStopChars!
+      })
+    }
+  }
+
+  const pointsByRow = incompleteRowsAddedChars.map((chars) => {
+    let points = 0
+    for (const char of chars.addedStopChars) {
+      switch (char) {
+        case ")":
+          points = points * 5 + 1
+          break
+
+        case "]":
+          points = points * 5 + 2
+          break
+
+        case "}":
+          points = points * 5 + 3
+          break
+        case ">":
+          points = points * 5 + 4
+          break
+      }
+    }
+    return { addedStopChars: chars.addedStopChars, points, row: chars.row }
+  })
+
+  console.log("Points by incomplete row:")
+  for (const row of pointsByRow) {
+    console.log("\n", row.row)
+    console.log(row.addedStopChars.join(""), "points:", row.points)
+  }
+
+  const sortedPoints = pointsByRow.sort((a, b) => a.points - b.points)
+
+  console.log("Sorted points:")
+  for (const row of sortedPoints) {
+    console.log(row.points)
+  }
+
+  const result = sortedPoints[Math.floor(pointsByRow.length / 2)].points
+
+  return { result }
+}
+
 /**
  * @see https://adventofcode.com/2021/day/10
  */
 export const day10_2021 = () => {
   console.log("2021 Day 10 puzzle 1 example:")
   const exampleData = loadFileRows(2021, 10, "example.txt")
-  const result1 = calculate(exampleData)
+  const result1 = findInvalidLinesAndCountPoints(exampleData)
   console.log("Result (should be 26397):", result1.result)
 
   console.log("\n2021 Day 10 puzzle 1:")
   const data = loadFileRows(2021, 10, "data.txt")
-  const result2 = calculate(data)
+  const result2 = findInvalidLinesAndCountPoints(data)
   console.log("Result:", result2.result)
+
+  console.log("\n2021 Day 10 puzzle 2 example:")
+  const result3 = findIncompleteLinesAndCountPoints(exampleData)
+  console.log("Result (should be 288957):", result3.result)
+
+  console.log("\n2021 Day 10 puzzle 2:")
+  const result4 = findIncompleteLinesAndCountPoints(data)
+  console.log("Result:", result4.result)
 }
